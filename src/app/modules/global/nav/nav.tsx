b5a982bff2ddb2bf31a2f7bebd0e98580d1ba0d8
Jep <jacob.dyke30@gmail.com>
2024-04-25 02:02:33 -0500

    rough hero

--- src/app/modules/global/nav/nav.tsx
+++ src/app/modules/global/nav/nav.tsx
@@ -4,9 +4,9 @@ import Logo from "@/app/media/logo-white.webp"
 export default function NavBar() {
 
     return (<>
-        <div className="h-20 flex flex-row w-full p-1 absolute">
+        <div className="h-20 flex flex-row w-full p-1 absolute z-20">
             <div className="w-1/2 h-full flex flex-row items-center ">
-                <Image alt="Marathon Capital Advisory Logo" src={Logo} className="ml-14"/>
+                <Image alt="Marathon Capital Advisory Logo" src={Logo} className="ml-14" />
 
             </div>
             <div className="w-1/2 ">
--- src/app/modules/homepage/hero.tsx
+++ src/app/modules/homepage/hero.tsx
@@ -1,25 +1,42 @@
 "use client"
-import NavBar from "../global/nav/nav"
-import { useEffect, useState } from "react"
+import Image from "next/image";
+import NavBar from "../global/nav/nav";
+import skyline from "@/app/media/skyline.webp";
+import street from "@/app/media/street-1.jpg";
+import dev from "@/app/media/development.jpg";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
-    const [text, scrollingText] = useState('Buy Side Advisory');
-    const [index, updateIndex] = useState(0);
-    const list = ['Buy Side Advisory', 'Corporate Advisory', 'Business Development'];
+    const desc1: string = "Servicing PE, VC, and debt facilities";
+    const desc2: string = "Fundraising at any stage of the business";
+    const desc3: string = "Utilizing network to drive revenue";
+    const [imageLocation, setLocation] = useState(street);
 
+    const [text, scrollingText] = useState('Buy Side Advisory');
+    const [desc, setDescription] = useState(desc1);
+    const [visible, setVisible] = useState(true);
+    let index = 1;
+    const titleList = ['Buy Side Advisory', 'Corporate Advisory', 'Business Development'];
+    const descList = [desc1, desc2, desc3];
+    const imgList = [street, skyline, dev];
+
+    setTimeout(() => {
+        setVisible(false);
+
+    }, 4500)
     useEffect(() => {
         const intervalID = setInterval(() => {
+            setVisible(true);
+            scrollingText(titleList[index]);
+            setDescription(descList[index]);
+            setLocation(imgList[index]);
+            index = (index + 1) % titleList.length;
 
-            if ((index + 1 % list.length) === 0) {
-                console.log('entered!')
-                updateIndex(0);
-            }
-            updateIndex(index + 1);
 
-            console.log(index);
-
-            scrollingText(list[index]);
-        }, 2000)
+            setTimeout(() => {
+                setVisible(false);
+            }, 4800)
+        }, 5000)
 
         return () => clearInterval(intervalID)
 
@@ -28,10 +45,12 @@ export default function Hero() {
 
     return (<>
         <NavBar />
-        <div className="h-screen bg-[url('media/skyline.webp')] bg-slate-700 bg-blend-overlay flex flex-col justify-center items-center">
-            <h1 className={`text-center text-white text-8xl`}>{text}</h1>
-            <button className="bg-white px-3 py-2 text-black w-fit text-center rounded-full" >Next</button>
+        <div className={` h-screen bg-no-repeat  flex flex-col justify-center items-center  `}>
+            <Image alt='Background images for scrolling hero' src={imageLocation} className={` ${visible ? ' opacity-100' : ' opacity-0'} transition-all duration-500 fixed w-full z-10 `} />
+            <div className="absolute top-0 left-0 bg-slate-700 h-full w-full z-10 opacity-75" />
+            <h1 className={`${visible ? ' opacity-100 scale-100 ' : ' opacity-0 scale-90 '} transition-all duration-500 text-center text-white text-8xl z-20`}>{text}</h1>
+            <h3 className={`text-white ${visible ? ' opacity-100 translate-y-0 ' : ' opacity-0 translate-y-10 '} delay-75 transition-all duration-500 text-xl p-5 z-20`}>{desc}</h3>
+            <button className={` ${visible ? ' opacity-100 translate-y-0 ' : ' opacity-0 translate-y-10 '} delay-150 transition-all duration-500 rounded-sm border-white border-2 py-3 px-5 text-white hover:bg-white hover:text-black z-20`}>Learn More</button>
         </div>
-
     </>)
 }
